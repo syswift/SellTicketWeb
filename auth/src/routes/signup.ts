e1c415c5a7adf4fbf1c3596fc47013bd1554498e
syswift <myworlddai97@gmail.com>
2020-08-04 22:18:13 +1200

    express validator

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -3,7 +3,11 @@ import express from 'express';
 const router = express.Router();
 
 router.post('/api/users/signup', (req, res) => {
-    res.send('Hi there!');
+    const { username, password } = req.body;
+
+    if(!username || typeof username !== 'string') {
+        res.status(400).send('Provide a valid email');
+    }
 });
 
 export { router as signupRouter};
