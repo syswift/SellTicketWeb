c48fb84f01409c94c13dd9bd84ad75123d271eac
syswift <myworlddai97@gmail.com>
2020-08-04 12:38:17 +1200

    google cloud

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,10 +4,10 @@ import { json } from 'body-parser';
 const app = express();
 app.use(json());
 
-app.get('api/users/currentuser', (req, res) => {
-    res.send('Hi there!');
-})
+app.get('/api/users/currentuser', (req, res) => {
+    res.send('Hi there bitch!!');
+});
 
 app.listen(3000, () => {
-    console.log('Listening on port 3000!');
-})
+    console.log('Listening on port 3000');
+});
