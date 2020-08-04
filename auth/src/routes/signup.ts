54ca923d1db47508352f3ab99e6b347e6d2e65ba
syswift <myworlddai97@gmail.com>
2020-08-04 22:44:01 +1200

    validator

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,13 +1,28 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
 
 const router = express.Router();
 
-router.post('/api/users/signup', (req, res) => {
-    const { username, password } = req.body;
+router.post('/api/users/signup',[
+    body('email')
+        .isEmail()
+        .withMessage('Email must be valid'),
+    body('password')
+        .trim()
+        .isLength({min: 8 , max: 20})
+        .withMessage('Password must be between 8 and 20 characters') 
+        //use express-validator
+], 
+(req: Request, res: Response) => {
+    const errors = validationResult(req);
 
-    if(!username || typeof username !== 'string') {
-        res.status(400).send('Provide a valid email');
-    }
+    const { email, password } = req.body;
+
+    if(!errors.isEmpty()){
+        return res.status(400).send(errors.array());
+    } 
+
+    console.log('Creating a user...');
 });
 
 export { router as signupRouter};
